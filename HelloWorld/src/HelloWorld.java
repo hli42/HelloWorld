b764071d1a93a084246995a758e3c52de1b2fc1b
Lianhao Qu <Lianhao Qu>
2014-01-18 00:59:38 -0500

    haha

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Greeting From Lianhao!!!");
+		System.out.println("Hello World");
+		System.out.println("Test #2");
+		System.out.println("Test 1/18/2014");
+	}
+}
