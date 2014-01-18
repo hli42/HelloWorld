51507a4cda85924458d8c56648612a88f9f45368
Heng Li <liheng620@gmail.com>
2014-01-18 00:46:50 -0500

    test 1/18/2014

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Test #2");
+		System.out.println("Test 1/18/2014");
 	}
 }
